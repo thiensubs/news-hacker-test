b8370fea07753bc32b7fc8809f1e529a8db1940b
Tien An <anvotien@gmail.com>
2020-02-21 15:44:28 +0700

    - Wating for 1st connection to cable.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     if params[:page].present?
       ParseNewsJob.perform_later(params[:page], params[:channel_id])
     else
-      @job_id = ParseNewsJob.perform_later().job_id 
+      @job_id = ParseNewsJob.set(wait: 4.seconds).perform_later().job_id 
     end
   end
 end
